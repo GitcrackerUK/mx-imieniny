86e85785f8a4d53fac83cc56896032108399bf58
mikEmix <michal@durooil.com>
2014-01-06 19:41:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+==================
+Wyświetla imieniny
---
+++ src/mxImieniny/Service.php
@@ -0,0 +1,83 @@
+<?php
+namespace mxImieniny;
+
+use DateTime;
+use InvalidArgumentException;
+
+class Service
+{
+    /** @var string */
+    public static $db;
+
+    /** @var array */
+    protected static $result;
+
+    /**
+     * @param string $month
+     * @param string $day
+     * @return       string
+     * @throws \InvalidArgumentException
+     */
+    public static function getByDate($month, $day)
+    {   
+        return self::find($month, $day);
+    }
+    
+    /**
+     * @param \DateTime $date
+     * @return          string
+     * @throws \InvalidArgumentException
+     */
+    public static function getByDateTime(DateTime $date)
+    {
+        return self::find($date->format('m'), $date->format('d'));
+    }
+    
+    /**
+     * @param  int $time
+     * @return string
+     * @throws \InvalidArgumentException
+     */
+    public static function getByTime($time)
+    {
+        return self::find(date('m', $time), date('d', $time));
+    }
+    
+    /**
+     * @param string $month
+     * @param string $day
+     * @return       string
+     * @throws \InvalidArgumentException
+     */
+    protected static function find($month, $day)
+    {
+        if (isset(self::$result[$month.$day])) {
+            return self::$result[$month.$day];
+        }
+    
+        $fp=fopen(self::getDatabase(), 'r');
+        while (! feof($fp)) {
+            if (fgets($fp, 5) == $day.$month) {
+                $line=str_replace(',', ', ', trim(fgets($fp, 500)));
+                self::$result[$month.$day]=$line;
+
+                fclose($fp);
+                return $line;
+            }
+        }
+        
+        throw new InvalidArgumentException('Nie znaleziono imienin dla podanej daty');
+    }
+    
+    /**
+     * @return string
+     */
+    protected static function getDatabase()
+    {
+        if (! self::$db) {
+            self::$db=__DIR__ . '/../data/imieniny.txt';
+        }
+
+        return self::$db;
+    }
+}
